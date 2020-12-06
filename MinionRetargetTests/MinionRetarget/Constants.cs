5059e38eafcdbbb50f6ee79d4862f9c1542c89ac
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-12-06 22:13:06 +0800

    Add unit tests

---
+++ MinionRetargetTests/MinionRetarget/Constants.cs
@@ -0,0 +1,41 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MinionRetargetTests.MinionRetarget
+{
+    [TestClass]
+    public class Constants
+    {
+        [TestMethod]
+        public void ModVer_Length_ReturnsCorrectFormat()
+        {
+            string result = Chen.MinionRetarget.MinionRetargetPlugin.ModVer;
+            const int ModVersionCount = 3;
+
+            int count = result.Split('.').Length;
+
+            Assert.AreEqual(ModVersionCount, count);
+        }
+
+        [TestMethod]
+        public void ModName_Value_ReturnsCorrectName()
+        {
+            string result = Chen.MinionRetarget.MinionRetargetPlugin.ModName;
+            const string ModName = "ChensMinionRetarget";
+
+            Assert.AreEqual(ModName, result);
+        }
+
+        [TestMethod]
+        public void ModGuid_Value_ReturnsCorrectGuid()
+        {
+            string result = Chen.MinionRetarget.MinionRetargetPlugin.ModGuid;
+            const string ModGuid = "com.Chen.ChensMinionRetarget";
+
+            Assert.AreEqual(ModGuid, result);
+        }
+    }
+}
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
---
+++ MinionRetargetTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MinionRetargetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinionRetargetTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc92f335-1392-4951-a1e9-1f28bfb894ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
