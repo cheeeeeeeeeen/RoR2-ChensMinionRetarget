221eb2597b31323a1f8898d3d6b039650cf390ce
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-12-07 02:12:01 +0800

    Update files after cleanup

--- ChensMinionRetarget.Tests/Properties/AssemblyInfo.cs
+++ ChensMinionRetarget.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ChensMinionRetarget.Tests")]
@@ -10,9 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("2a501e37-35cf-41ed-bae2-0bcb4afb78ed")]
 
 // [assembly: AssemblyVersion("1.0.*")]
